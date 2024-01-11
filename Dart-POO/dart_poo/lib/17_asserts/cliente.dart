e1b638ad091ed197a7fbf24b7dee97150bfb88eb
Yuri Lima <yurilima742@yahoo.com>
2024-01-11 16:37:17 -0300

    refactor: refatorando exemplos de asserts em POO

--- Dart-POO/dart_poo/lib/17_asserts/cliente.dart
+++ Dart-POO/dart_poo/lib/17_asserts/cliente.dart
@@ -10,6 +10,16 @@ class Cliente {
     this.cpf,
     this.cnpj,
   }) : 
-  assert((cpf != null) ? nome != null && razaoSocial == null && cnpj == null : true, 'Você enviou cpf junto com cnpj'),
-  assert((cnpj != null) ? razaoSocial == null && nome == null && cpf == null : true, 'Você enviou cnpj junto com cpf');
+  assert(
+    (cpf != null)
+        ? nome != null && razaoSocial == null && cnpj == null
+        : true,
+    'Você enviou cpf junto com cnpj',
+  ),
+  assert(
+    (cnpj != null)
+        ? razaoSocial == null && nome == null && cpf == null
+        : true,
+    'Você enviou cnpj junto com cpf',
+  );
 }
