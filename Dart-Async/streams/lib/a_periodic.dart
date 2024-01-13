42e5bc5e33e35420370e4f824739a2a3799b3199
Yuri Lima <yurilima742@yahoo.com>
2024-01-13 19:27:49 -0300

    feat: adicionando módulo de streams

---
+++ Dart-Async/streams/lib/a_periodic.dart
@@ -0,0 +1,14 @@
+Future<void> main() async {
+  final interval = Duration(seconds: 2);
+  final stream = Stream<int>.periodic(interval, callback);
+
+  // await for
+  await for (var i in stream) {
+    print(i);
+  }
+}
+
+int callback(int value) {
+  print('Valor = $value');
+  return (value + 1) * 2;
+}
