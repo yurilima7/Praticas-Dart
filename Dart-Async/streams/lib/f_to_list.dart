ae280d738ee48f40766dca5483e3ea7254bf102a
Yuri Lima <yurilima742@yahoo.com>
2024-01-15 09:44:45 -0300

    feat: adicionando toList de stream

--- Dart-Async/streams/lib/f_to_list.dart
+++ Dart-Async/streams/lib/f_to_list.dart
@@ -2,11 +2,10 @@ Future<void> main() async {
   final interval = Duration(seconds: 2);
   var stream = Stream<int>.periodic(interval, callback);
 
-  stream = stream.skip(5).skip(2);
-  // await for
-  await for (var i in stream) {
-    print(i);
-  }
+  stream = stream.take(5);
+
+  final data = await stream.toList();
+  print(data);
 }
 
 int callback(int value) {
