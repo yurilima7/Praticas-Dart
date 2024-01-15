cbf76c174d63dcb677829c83c69cfc98af03c4c1
Yuri Lima <yurilima742@yahoo.com>
2024-01-15 09:41:28 -0300

    refactor: corrigindo skip de stream d e f

--- Dart-Async/streams/lib/e_skip_while.dart
+++ Dart-Async/streams/lib/e_skip_while.dart
@@ -2,7 +2,7 @@ Future<void> main() async {
   final interval = Duration(seconds: 2);
   var stream = Stream<int>.periodic(interval, callback);
 
-  stream = stream.skip(5).skip(2);
+  stream = stream.skip(5).skipWhile((numero) => numero < 5,);
   // await for
   await for (var i in stream) {
     print(i);
